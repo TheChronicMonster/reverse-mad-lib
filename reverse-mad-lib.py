55727e7fb06c1a44d823b03093d8f64954a96e8f
JP Miller <legendzing@gmail.com>
2015-10-05 19:07:23 -0500

    feat: working program
    answers fill in after each user input

--- reverse-mad-lib.py
+++ reverse-mad-lib.py
@@ -1,11 +1,10 @@
-### Build MadLibs Reverse Game ###
-# STEPS #
-# if answered correctly sentence refreshes with correct answer in place #
+### MadLibs Reverse Game ###
+
+# It's not as fun as a crazy Mad Lib game, but is a bit more educational.
 
 ### RESOURCES ###
+# UDACITY IPND Madlib Project 2
 # python mad lib youtube https://www.youtube.com/watch?v=Oq75BBqd2y0
-
-### User selects difficulty
 
 #Global Variables#
 difficulty = ""
@@ -13,16 +12,7 @@ easy = "The programming __1__ known as __2__ is an interpreted __1__. That means
 medium = "A __5__ __6__ repeats a statment while a given condition is __7__. A __8__ __6__ is used to repeat code n __9__ of times. Please note, __6__s break by using the __10__ command."
 hard = "A __11__ is a block of organized, reusable __12__ that is used to perform a single, related action. A __11__ __13__ begins with the __14__ __15__ followed by the __11__ name and __16__. Any __17__ should be placed within the __16__."
 
-# easy = "easy"
-# medium = "medium"
-# hard = "hard"
-
-#replaceMe = ["__" + str(questionNumber) + "__"]
 answersToQuestions = ["language", "Python", "program", "object", "while","loop","True","for","number","return","function","code","block","keyword","def","parentheses","parameters"]
-print answersToQuestions
-print len(answersToQuestions)
-
-#print replaceMe
 
 print "What difficulty would you like to play?"
 def difficultySelector(difficulty):
@@ -53,7 +43,8 @@ def askAndAnswerQuestions(difficulty, answer, questionsToAnswer):
 		user_input = raw_input("Which word replaces number " + str(answer + 1) + "?: ")
 		if user_input == answersToQuestions[answer]:
 			## Update sentence with replaced answer ##
-			print  difficulty.replace("__" + str(answer+1) + "__", answersToQuestions[answer])
+			difficulty = difficulty.replace("__" + str(answer+1) + "__", answersToQuestions[answer])
+			print difficulty
 			answer += 1
 		else:
 			print "Try again"
